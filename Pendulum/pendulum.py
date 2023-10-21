1e2713656784e7235f66148db9361bdadb3b3cb2
huyenanh-nguyen <ng_huyenanh@mailbox.org>
2023-10-21 21:34:03 +0200

    pwd

---
+++ Pendulum/pendulum.py
@@ -0,0 +1,16 @@
+from MathKit.statsengine import Statistics
+import pandas as pd
+import numpy as np
+from numpy import sqrt
+import matplotlib.pyplot as plt
+from pathlib import Path
+from scipy.interpolate import approximate_taylor_polynomial
+from scipy.optimize import curve_fit
+
+class Pendulum:
+    """
+    random functions to keep my Notebook clean.
+    like plotting the plot or searching the data in the excel file
+    """
+
+    
