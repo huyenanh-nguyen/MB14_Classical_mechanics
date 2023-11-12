c89ccaa678cdfa6f9cbf63efadaae3051579368f
huyenanh-nguyen <ng_huyenanh@mailbox.org>
2023-11-12 19:58:04 +0100

    guitarstring

--- String Vibration/stringvibration.py
+++ String Vibration/stringvibration.py
@@ -2,3 +2,9 @@ import numpy as np
 import pandas as pd
 import openpyxl
 from pathlib import Path, Purepath
+
+class Guitarstring:
+    """
+    Observing transversal waves on a string, which is fixed at both ends.
+    
+    """
