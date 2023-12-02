db19612f36e73f737a7bfc9e0268a3c1a7df4cf3
huyenanh-nguyen <ng_huyenanh@mailbox.org>
2023-12-02 12:38:19 +0100

    task 4

---
+++ String_Vibration/runtask4.py
@@ -0,0 +1,34 @@
+import argparse
+from pathlib import Path, PurePath
+from stringvibration import Guitarstring
+from MathKit import Statistics
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+from matplotlib.ticker import FormatStrFormatter
+from scipy.optimize import curve_fit
+
+def linearfit(x, m):
+    return m * x 
+
+# terminal script:
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description = "String Vibration")
+    parser.add_argument("excelpath", type = str, help = "Path to the Excelfile -> M12_Saitenschwingung.xlsx")
+    parser.add_argument("taskindex", type = int, help = "index of the task -> Task1 = 0 & 1, Task2 = 2, Task3 = 3, Task 4 = 4")
+    parser.add_argument("x_column", type = str, help = "column name of the x values -> n is mostly the x_column")
+    parser.add_argument("y_column", type = str, help = "column name of the y values -> fn is mostly the y_column")
+    parser.add_argument("valueset", type = str, help = "name of the Valueset (like : fn in Hz)")
+    parser.add_argument("systematic_error_frequence", type = float, help = "systematic error of the measuring tool -> ±0.02 Hz of the oscilloscop, 0.01 m of the ruler ")
+    parser.add_argument("systematic_error_ruler", type = float, help = "systematic error of the measuring tool -> ±0.02 Hz of the oscilloscop, 0.01 m of the ruler ")
+    args = parser.parse_args()  # collects all arguments
+
+
+    excelpath = PurePath(str(Path.cwd()) + "/" + (args.excelpath))
+    taskindex = args.taskindex
+    x_column = args.x_column
+    y_column = args.y_column
+    valueset = args.valueset
+    systematic_error_frequence = args.systematic_error_frequence
+    systematic_error_ruler = args.systematic_error_ruler
