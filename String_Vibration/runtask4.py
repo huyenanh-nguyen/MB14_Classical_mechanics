76e08b7702481ee6b20be7e21d0a6ef22b5cf3a5
huyenanh-nguyen <ng_huyenanh@mailbox.org>
2023-12-31 15:04:29 +0100

    finally found the issue in task4

--- String_Vibration/runtask4.py
+++ String_Vibration/runtask4.py
@@ -39,38 +39,50 @@ if __name__ == "__main__":
     roundnum = 3
     string = Guitarstring(excelpath)
     data = string.excel_dataframes()[taskindex]
-    x_value = data["F0 in N"] 
-    y_value = [np.sqrt(data["f1 in Hz"][i]) for i in range(len(x_value))]
+    y_value = data["f1 in Hz"]
+    x_value = [np.sqrt(data["F0 in N"][i]) for i in range(len(y_value))]
 
+    
     print(string.resonancefit_params(taskindex, x_column, y_column))
 
-    popt, pcov = curve_fit(linearfit, x_value, y_value)
-    residuals = y_value - linearfit(np.asarray(x_value), *popt)
-    ss_res = np.sum(residuals ** 2)
-    ss_total = np.sum((y_value - np.mean(y_value)) ** 2)
-    r_square = 1 - (ss_res / ss_total)
 
 
-    ppt, cov = curve_fit(originfit, x_value, y_value)
-
-    print("intercept:", popt, "std:", np.sqrt(np.diag(pcov)), r_square)
-    print("_____________________________________")
-    print("origin:",ppt, "std:", np.sqrt(np.diag(cov)))
+    
 
 
 
-    fig = plt.figure()
 
-    plt.scatter(x = x_value, y = y_value, marker = ".")
-    plt.plot(x_value, originfit(x_value, np.array(ppt[0])), color = "tab:orange")
-    plt.plot(x_value, linearfit(x_value, np.array(popt[0]), np.array(popt[1])), color = "tab:blue")
 
-    # ax.set_ylim(ymin=0)
-    # ax.set_xlim(xmin=0)
-    plt.legend(loc = 'upper left')
-    plt.xlabel("L in m", fontsize=12)
-    plt.ylabel(r'$\frac{1}{f_1}$ in $\frac{1}{Hz}$', fontsize=12)
 
-    plt.show()
 
-    # python3 runtask4.py M12_Saitenschwingung.xlsx 4 "F0 in N" "f1 in Hz" "f1 in Hz" 0.03 0.01
+
+    # popt, pcov = curve_fit(linearfit, x_value, y_value)
+    # residuals = y_value - linearfit(np.asarray(x_value), *popt)
+    # ss_res = np.sum(residuals ** 2)
+    # ss_total = np.sum((y_value - np.mean(y_value)) ** 2)
+    # r_square = 1 - (ss_res / ss_total)
+
+
+    # ppt, cov = curve_fit(originfit, x_value, y_value)
+
+    # print("intercept:", popt, "std:", np.sqrt(np.diag(pcov)), r_square)
+    # print("_____________________________________")
+    # print("origin:",ppt, "std:", np.sqrt(np.diag(cov)))
+
+
+
+    # fig = plt.figure()
+
+    # plt.scatter(x = x_value, y = y_value, marker = ".")
+    # plt.plot(x_value, originfit(x_value, np.array(ppt[0])), color = "tab:orange")
+    # plt.plot(x_value, linearfit(x_value, np.array(popt[0]), np.array(popt[1])), color = "tab:blue")
+
+    # # ax.set_ylim(ymin=0)
+    # # ax.set_xlim(xmin=0)
+    # plt.legend(loc = 'upper left')
+    # plt.xlabel(r'$\sqrt{F_0}$ in $\sqrt{N}$', fontsize=12)
+    # plt.ylabel(r'$f_1$ in Hz', fontsize=12)
+
+    # plt.show()
+
+    # python3 runtask4.py M12_Saitenschwingung.xlsx 4 "sqrt(F0) in sqrt(N)" "f1 in Hz" "f1 in Hz" 0.03 0.01
