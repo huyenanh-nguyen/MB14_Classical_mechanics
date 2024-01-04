d4e6a1d3de854d3be76f977b7209459529f4908d
huyenanh-nguyen <ng_huyenanh@mailbox.org>
2024-01-04 20:08:52 +0100

    init

--- Wheatstone_bridge/wheatstone_bridge.py
+++ Wheatstone_bridge/wheatstone_bridge.py
@@ -4,3 +4,12 @@ import openpyxl
 from pathlib import Path, PurePath
 from scipy.optimize import curve_fit
 from MathKit import Statistics
+
+class Wheatstone:
+    
+    def __init__(self, excelpath):
+        """
+        Args:
+            excelpath (Path): PAth to my excelfile
+        """
+        self.excelpath = excelpath
